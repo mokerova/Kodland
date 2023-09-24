1012238d196f6e62d4cedb01c4237543a6faedbe
mokerova <45970745+mokerova@users.noreply.github.com>
2023-09-24 13:30:36 +0300

    Create bot.py

---
+++ bot.py
@@ -0,0 +1,39 @@
+import random
+import tok
+import discord
+from discord.ext import commands
+
+intents = discord.Intents.default()
+intents.message_content = True
+
+bot = commands.Bot(command_prefix='$', intents=intents)
+
+def gen_pass(pass_length):
+    elements = "+-/*!&$#?=@abcdefghijklnopqrstuvwxyzABCDEFGHIJKLMNOPQRSTUVWXYZ1234567890"
+    password = ""
+    for i in range(pass_length):
+        password += random.choice(elements)
+    return password
+
+@bot.command()
+async def add(ctx, left: int, right: int):
+    """Adds two numbers together."""
+    await ctx.send(left + right)
+
+@bot.event
+async def on_ready():
+    print(f'We have logged in as {bot.user}')
+
+@bot.command()
+async def hello(ctx):
+    await ctx.send(f'Привет! Я бот {bot.user}!')
+
+@bot.command()
+async def bye(ctx):
+    await ctx.send(f'Пока!')
+
+@bot.command()
+async def pasw(ctx, count=8):
+    await ctx.send(f'Ваш пароль - {gen_pass(count)}')
+
+bot.run(tok.TOKEN)
